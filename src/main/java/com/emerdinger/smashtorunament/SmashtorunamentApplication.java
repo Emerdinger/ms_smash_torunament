eef7df76d17e963f226f36edafd70256560581f0
Emerson Giovanni Nino Parada <egnino@bancolombia.com.co>
2024-09-10 10:20:21 -0500

    Base Java Project

---
+++ src/main/java/com/emerdinger/smashtorunament/SmashtorunamentApplication.java
@@ -0,0 +1,13 @@
+package com.emerdinger.smashtorunament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashtorunamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashtorunamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerdinger/smashtorunament/SmashtorunamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerdinger.smashtorunament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashtorunamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
